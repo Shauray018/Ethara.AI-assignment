77cbbf42b4ff347fe9d422c5d65ea4dd53ce4c3e
Shauray018 <shauraydhingra03@gmail.com>
2026-05-03 18:17:39 +0530

    fix: fixed build errors

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -1,7 +1,9 @@
 import { PrismaPg } from "@prisma/adapter-pg"
-import { PrismaClient } from "@prisma/client"
+import prismaClient from "@prisma/client"
 
 import { env } from "./env.js"
+
+const { PrismaClient } = prismaClient
 
 const adapter = new PrismaPg({
   connectionString: env.DATABASE_URL,
